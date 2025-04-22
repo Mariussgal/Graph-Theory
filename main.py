e0669dbd5697f9cd8f71ba4b38f2ad157b3d966c
mariussgal <marius.gal@esme.fr>
2025-04-22 13:46:03 +0200

    used  rich lib to enhance UX/UI

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 from src.graph_utils import analyze_toy_dataset, analyze_karate_club_dataset, analyze_student_dataset, analyze_anybeatAnonymized_dataset
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from rich.prompt import Prompt
+from rich.align import Align
+
+
+console = Console()
+console_width = console.width
 
 def show_menu():
-    print("\n" + "="*50)
-    print("Social Network Analysis Tool")
-    print("="*50)
-    print("1. Load and analyze Toy dataset")
-    print("2. Load and analyze Karate Club dataset")
-    print("3. Load and analyze Student cooperation dataset")
-    print("4. Load and analyze Anybeat dataset")
-    print("0. Quit")
-    print("="*50)
-    return input("Enter your choice (0-4): ")
+    console.print("\n" * 2)
+    
+    print("\n" + "=" * console_width)
+    console.print(Align.center(Panel(
+        "[bold blue]🔍 Social Network Analysis Tool[/bold blue]",
+        border_style="blue",
+        padding=(1, 3, 1, 3),
+        width=40
+         
+    ))
+    )
+    console.print("=" * console_width)
+    console.print("\n"*2)
+
+    table = Table(show_header=False, box=None)
+    table.add_column("Option", style="cyan", no_wrap=True)
+    table.add_column("Description", style="white")
+
+    table.add_row("1", "Load and analyze Toy dataset")
+    table.add_row("2", "Load and analyze Karate Club dataset")
+    table.add_row("3", "Load and analyze Student cooperation dataset")
+    table.add_row("4", "Load and analyze Anybeat dataset")
+    table.add_row("0", "Quit")
+    
+
+    console.print(table)
+    console.print("\n")
+    return Prompt.ask("[bold cyan]Enter your choice[/bold cyan]", choices=["0", "1", "2", "3", "4"], default="0")
+
+
 
 def main():
     while True:
         choice = show_menu()
         if choice == '0':
-            print("\n See you!")
+            console.print("\n[bold blue]Thank you for using Social Network Analysis Tool! See you soon! 👋[/bold blue]\n")
             break
         elif choice == '1':
             analyze_toy_dataset()
